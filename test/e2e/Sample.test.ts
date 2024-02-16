7ca6e4f2b293cb21cd8eeca048d8d1e6f9bfbf38
Henry <11198460+godzillaba@users.noreply.github.com>
2024-02-16 12:25:53 -0500

    custom fee l3

---
+++ test/e2e/Sample.test.ts
@@ -0,0 +1,37 @@
+import { assert } from 'chai'
+import { testSetup } from '../../lib/arbitrum-sdk/scripts/testSetup'
+import { Wallet, ethers } from 'ethers'
+import { L1Network, L2Network, getL1Network } from '../../lib/arbitrum-sdk/src'
+
+function getEnv(name: string): string {
+  const value = process.env[name]
+  if (!value) {
+    throw new Error(`Missing env variable: ${name}`)
+  }
+  return value
+}
+
+const l1Signer = new Wallet(getEnv('LOCAL_L1_KEY'), new ethers.providers.JsonRpcProvider(getEnv('LOCAL_L1_RPC')))
+const l2Signer = new Wallet(getEnv('LOCAL_L2_KEY'), new ethers.providers.JsonRpcProvider(getEnv('LOCAL_L2_RPC')))
+const l3Signer = new Wallet(getEnv('LOCAL_L3_KEY'), new ethers.providers.JsonRpcProvider(getEnv('LOCAL_L3_RPC')))
+
+describe('E2E Sample', () => {
+  let l1Network: L1Network
+  let l2Network: L2Network
+  let l3Network: L2Network
+
+  before(async () => {
+    // setup.l1Network is actually the L2 network
+    const setup = await testSetup()
+
+    l1Network = await getL1Network((setup.l1Network as L2Network).partnerChainID)
+    l2Network = setup.l1Network as L2Network
+    l3Network = setup.l2Network
+  })
+
+  it("sample test", async () => {
+    assert((await l1Signer.getBalance()).gt(0))
+    assert((await l2Signer.getBalance()).gt(0))
+    assert((await l3Signer.getBalance()).gt(0))
+  })
+})
