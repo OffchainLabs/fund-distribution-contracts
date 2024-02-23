b7dc9c6a1ca0bd725546243546d0d8ad81731ece
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-02-23 16:07:50 -0500

    update arb deploy script

--- script/FeeRouterDeployer/Sepolia/arb-sepolia-deploy.s.sol
+++ script/FeeRouterDeployer/Sepolia/arb-sepolia-deploy.s.sol
@@ -1,12 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
 import "forge-std/Script.sol";
 import "../../../src/FeeRouter/ChildToParentRewardRouter.sol";
 
 contract DeployScript is Script {
     function run() public {
         // deployed via sepolia-deploy.s.sol
-        address parentChainTarget = 0xbFC9513A767F226a5878072E1D7C3bF663D9e297;
+         address parentChainTarget = vm.envAddress("SEP_PARENT_CHAIN_TARGET");
+
         uint256 minDistributionIntervalSeconds = 60;
-        address childChainGatewayRouter = address(0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7);
+        address childChainGatewayRouter = 0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7;
         vm.startBroadcast();
         ChildToParentRewardRouter router = new ChildToParentRewardRouter({
             _parentChainTarget: parentChainTarget,
