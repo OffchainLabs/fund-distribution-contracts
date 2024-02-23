a644fa8cab20fbe2be5399b59075f4b502e2bc69
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-02-23 09:58:21 -0500

    update deployer scripts

--- script/FeeRouterDeployer/NovaToDAOTreasury/eth-deploy.s.sol
+++ script/FeeRouterDeployer/NovaToDAOTreasury/eth-deploy.s.sol
@@ -5,28 +5,29 @@ import "forge-std/Script.sol";
 import "../../../src/FeeRouter/ParentToChildRewardRouter.sol";
 
 contract DeployScript is Script {
-    function setUp() public {}
+function setUp() public {}
 
-    function run() public {
-        // on L1:
-        address novaInbox = 0xc4448b71118c9071Bcb9734A0EAc55D18A153949;
-        // on Arb One:
-        address daoTreasuryTimelock = 0xbFc1FECa8B09A5c5D3EFfE7429eBE24b9c09EF58;
+function run() public {
+    // on L1:
+    address parentChainGatewayRouter = address(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef); 
+    
+    // on Arb One:
+    address daoTreasuryTimelock = 0xbFc1FECa8B09A5c5D3EFfE7429eBE24b9c09EF58;
 
-        uint256 minDistributionIntervalSeconds = 7 days;
-        uint256 minGasPrice = 0.1 gwei;
-        uint minGasLimit = 120_000;
-        vm.startBroadcast();
-        ParentToChildRewardRouter router = new ParentToChildRewardRouter({
-            _inbox: novaInbox,
-            _destination: daoTreasuryTimelock,
-            _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
-            _minGasPrice: minGasPrice,
-            _minGasLimit: minGasLimit
-        });
+    uint256 minDistributionIntervalSeconds = 7 days;
+    uint256 minGasPrice = 0.1 gwei;
+    uint minGasLimit = 120_000;
+    vm.startBroadcast();
+    ParentToChildRewardRouter router = new ParentToChildRewardRouter({
+        _parentChainGatewayRouter: IParentChainGatewayRouter(parentChainGatewayRouter),
+        _destination: daoTreasuryTimelock,
+        _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
+        _minGasPrice: minGasPrice,
+        _minGasLimit: minGasLimit
+    });
 
-        console.log("Deployed ParentToChildRewardRouter onto Ethereum at");
-        console.log(address(router));
-        vm.stopBroadcast();
-    }
+    console.log("Deployed ParentToChildRewardRouter onto Ethereum at");
+    console.log(address(router));
+    vm.stopBroadcast();
+}
 }
--- script/FeeRouterDeployer/Sepolia/sepolia-deploy.s.sol
+++ script/FeeRouterDeployer/Sepolia/sepolia-deploy.s.sol
@@ -5,28 +5,28 @@ import "forge-std/Script.sol";
 import "../../../src/FeeRouter/ParentToChildRewardRouter.sol";
 
 contract DeployScript is Script {
-    function setUp() public {}
+function setUp() public {}
 
-    function run() public {
-        address arbSepoliaInbox = 0xaAe29B0366299461418F5324a79Afc425BE5ae21;
+function run() public {
+    address parentChainGatewayRouter = address(0xcE18836b233C83325Cc8848CA4487e94C6288264); 
 
-        // this is just an EOA:
-        address arbSepoliaDestination = 0x7E43B9cE022f6127c739957550F94c64EC2A604A;
+    // this is just an EOA:
+    address arbSepoliaDestination = 0x7E43B9cE022f6127c739957550F94c64EC2A604A;
 
-        uint256 minDistributionIntervalSeconds = 60;
-        uint256 minGasPrice = 0.1 gwei;
-        uint minGasLimit = 100_000;
-        vm.startBroadcast();
-        ParentToChildRewardRouter router = new ParentToChildRewardRouter({
-            _inbox: arbSepoliaInbox,
-            _destination: arbSepoliaDestination,
-            _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
-            _minGasPrice: minGasPrice,
-            _minGasLimit: minGasLimit
-        });
+    uint256 minDistributionIntervalSeconds = 60;
+    uint256 minGasPrice = 0.1 gwei;
+    uint minGasLimit = 100_000;
+    vm.startBroadcast();
+    ParentToChildRewardRouter router = new ParentToChildRewardRouter({
+        _destination: arbSepoliaDestination,
+        _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
+        _minGasPrice: minGasPrice,
+        _minGasLimit: minGasLimit,
+        _parentChainGatewayRouter: IParentChainGatewayRouter(parentChainGatewayRouter)
+    });
 
-        console.log("Deployed ParentToChildRewardRouter onto Sepolia at");
-        console.log(address(router));
-        vm.stopBroadcast();
-    }
+    console.log("Deployed ParentToChildRewardRouter onto Sepolia at");
+    console.log(address(router));
+    vm.stopBroadcast();
+}
 }
