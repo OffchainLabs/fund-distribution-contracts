37a000b1458f446cd44ecd0cdcf0309d29cbd060
Daniel Goldman <dzgoldman@wesleyan.edu>
2024-02-23 12:02:23 -0500

    update ParentToChildRewardRouter test

--- test/ParentToChildRewardRouter.t.sol
+++ test/ParentToChildRewardRouter.t.sol
@@ -5,92 +5,94 @@ import "forge-std/Test.sol";
 import "./util/InboxMock.sol";
 import "../src/FeeRouter/ParentToChildRewardRouter.sol";
 
-// TODO: update
-// contract ParentToChildRewardRouterTest is Test {
-//     ParentToChildRewardRouter parentToChildRewardRouter;
-//     InboxMock inbox;
-//     address me;
+contract ParentToChildRewardRouterMock {
+    InboxMock inboxMock;
 
-//     uint256 minDistributionIntervalSeconds = 20;
+    constructor(InboxMock _inboxMock) {
+        inboxMock = _inboxMock;
+    }
 
-//     function setUp() public {
-//         inbox = new InboxMock();
-//         parentToChildRewardRouter = new ParentToChildRewardRouter({
-//             _inbox: address(inbox),
-//             _destination: address(1234),
-//             _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
-//             _minGasPrice: 1,
-//             _minGasLimit: 1 ether
-//         });
-//         vm.deal(me, 10 ether);
-//     }
+    function inbox() external view returns (InboxMock) {
+        return inboxMock;
+    }
+}
 
-//     function testrouteNativeFunds() external {
-//         vm.startPrank(me);
-//         (bool sent, ) = address(parentToChildRewardRouter).call{value: 1 ether}(
-//             ""
-//         );
-//         assertTrue(sent, "funds sent");
-//         assertEq(
-//             address(parentToChildRewardRouter).balance,
-//             1 ether,
-//             "funds received"
-//         );
-//         assertEq(inbox.msgNum(), 0, "inbox msg num 0");
-//         parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
-//             maxSubmissionCost: 1 ether,
-//             gasLimit: 1 ether,
-//             maxFeePerGas: 1
-//         });
+contract ParentToChildRewardRouterTest is Test {
+    ParentToChildRewardRouter parentToChildRewardRouter;
+    InboxMock inbox;
+    address me;
 
-//         assertEq(inbox.msgNum(), 1, "create retryable ticket called");
-//         assertEq(address(parentToChildRewardRouter).balance, 0, "funds routed");
+    uint256 minDistributionIntervalSeconds = 20;
 
-//         vm.stopPrank();
-//     }
+    function setUp() public {
+        inbox = new InboxMock();
+        ParentToChildRewardRouterMock parentToChildRewardRouterMock = new ParentToChildRewardRouterMock(inbox);
+        parentToChildRewardRouter = new ParentToChildRewardRouter({
+            _parentChainGatewayRouter: IParentChainGatewayRouter(address(parentToChildRewardRouterMock)),
+            _destination: address(1234),
+            _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
+            _minGasPrice: 1,
+            _minGasLimit: 1 ether
+        });
+        vm.deal(me, 10 ether);
+    }
 
-//     function testRevertsWithInsufficientValue() external {
-//         vm.startPrank(me);
+    function testrouteNativeFunds() external {
+        vm.startPrank(me);
+        (bool sent,) = address(parentToChildRewardRouter).call{value: 1 ether}("");
+        assertTrue(sent, "funds sent");
+        assertEq(address(parentToChildRewardRouter).balance, 1 ether, "funds received");
+        assertEq(inbox.msgNum(), 0, "inbox msg num 0");
+        parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
+            maxSubmissionCost: 1 ether,
+            gasLimit: 1 ether,
+            maxFeePerGas: 1
+        });
 
-//         (bool sent, bytes memory data) = address(parentToChildRewardRouter)
-//             .call{value: 1 ether}("");
-//         assertTrue(sent, "funds sent");
+        assertEq(inbox.msgNum(), 1, "create retryable ticket called");
+        assertEq(address(parentToChildRewardRouter).balance, 0, "funds routed");
 
-//         vm.expectRevert(
-//             abi.encodeWithSelector(IncorrectValue.selector, 2 ether, 1.9 ether)
-//         );
-//         parentToChildRewardRouter.routeNativeFunds{value: 1.9 ether}({
-//             maxSubmissionCost: 1 ether,
-//             gasLimit: 1 ether,
-//             maxFeePerGas: 1
-//         });
-//         assertEq(inbox.msgNum(), 0, "create retryable ticket not called");
+        vm.stopPrank();
+    }
 
-//         vm.stopPrank();
-//     }
-//     function testCantRouteTooSoon() public {
-//         vm.startPrank(me);
-//         (bool sent, ) = address(parentToChildRewardRouter).call{value: 1 ether}(
-//             ""
-//         );
-//         assertTrue(sent, "funds sent");
-//         parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
-//             maxSubmissionCost: 1 ether,
-//             gasLimit: 1 ether,
-//             maxFeePerGas: 1
-//         });
-//         vm.expectRevert(
-//             abi.encodeWithSelector(
-//                 DistributionTooSoon.selector,
-//                 block.timestamp,
-//                 parentToChildRewardRouter.nextDistribution()
-//             )
-//         );
-//         parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
-//             maxSubmissionCost: 1 ether,
-//             gasLimit: 1 ether,
-//             maxFeePerGas: 1
-//         });
-//         vm.stopPrank();
-//     }
-// }
+    function testRevertsWithInsufficientValue() external {
+        vm.startPrank(me);
+
+        (bool sent, bytes memory data) = address(parentToChildRewardRouter).call{value: 1 ether}("");
+        assertTrue(sent, "funds sent");
+
+        vm.expectRevert(abi.encodeWithSelector(IncorrectValue.selector, 2 ether, 1.9 ether));
+        parentToChildRewardRouter.routeNativeFunds{value: 1.9 ether}({
+            maxSubmissionCost: 1 ether,
+            gasLimit: 1 ether,
+            maxFeePerGas: 1
+        });
+        assertEq(inbox.msgNum(), 0, "create retryable ticket not called");
+
+        vm.stopPrank();
+    }
+
+    function testCantRouteTooSoon() public {
+        vm.startPrank(me);
+        (bool sent,) = address(parentToChildRewardRouter).call{value: 1 ether}("");
+        assertTrue(sent, "funds sent");
+        parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
+            maxSubmissionCost: 1 ether,
+            gasLimit: 1 ether,
+            maxFeePerGas: 1
+        });
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                DistributionInterval.DistributionTooSoon.selector,
+                block.timestamp,
+                parentToChildRewardRouter.nextDistributions(parentToChildRewardRouter.NATIVE_CURRENCY())
+            )
+        );
+        parentToChildRewardRouter.routeNativeFunds{value: 2 ether}({
+            maxSubmissionCost: 1 ether,
+            gasLimit: 1 ether,
+            maxFeePerGas: 1
+        });
+        vm.stopPrank();
+    }
+}
