00f2491437d7053c093a0d707b1a0f31568588e8
Henry <11198460+godzillaba@users.noreply.github.com>
2024-02-23 10:48:57 -0500

    format

--- script/FeeRouterDeployer/NovaToDAOTreasury/nova-deploy.s.sol
+++ script/FeeRouterDeployer/NovaToDAOTreasury/nova-deploy.s.sol
@@ -2,20 +2,20 @@ import "forge-std/Script.sol";
 import "../../../src/FeeRouter/ChildToParentRewardRouter.sol";
 
 contract DeployScript is Script {
-function run() public {
-    // deployed via eth-deploy.s.sol:
-    address parentChainTarget = vm.envAddress("L1_TO_DAO_TIMELOCK_ROUTER");
-    uint256 minDistributionIntervalSeconds = 7 days;
-    address childChainGatewayRouter = address(0x21903d3F8176b1a0c17E953Cd896610Be9fFDFa8);
+    function run() public {
+        // deployed via eth-deploy.s.sol:
+        address parentChainTarget = vm.envAddress("L1_TO_DAO_TIMELOCK_ROUTER");
+        uint256 minDistributionIntervalSeconds = 7 days;
+        address childChainGatewayRouter = address(0x21903d3F8176b1a0c17E953Cd896610Be9fFDFa8);
 
-    vm.startBroadcast();
-    ChildToParentRewardRouter router = new ChildToParentRewardRouter({
-        _parentChainTarget: parentChainTarget,
-        _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
-        _childChainGatewayRouter: IChildChainGatewayRouter(childChainGatewayRouter)
-    });
-    console.log("Deployed ChildToParentRewardRouter onto nova at");
-    console.log(address(router));
-    vm.stopBroadcast();
-}
+        vm.startBroadcast();
+        ChildToParentRewardRouter router = new ChildToParentRewardRouter({
+            _parentChainTarget: parentChainTarget,
+            _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
+            _childChainGatewayRouter: IChildChainGatewayRouter(childChainGatewayRouter)
+        });
+        console.log("Deployed ChildToParentRewardRouter onto nova at");
+        console.log(address(router));
+        vm.stopBroadcast();
+    }
 }
--- script/FeeRouterDeployer/Sepolia/arb-sepolia-deploy.s.sol
+++ script/FeeRouterDeployer/Sepolia/arb-sepolia-deploy.s.sol
@@ -2,19 +2,19 @@ import "forge-std/Script.sol";
 import "../../../src/FeeRouter/ChildToParentRewardRouter.sol";
 
 contract DeployScript is Script {
-function run() public {
-    // deployed via sepolia-deploy.s.sol
-    address parentChainTarget = 0xbFC9513A767F226a5878072E1D7C3bF663D9e297;
-    uint256 minDistributionIntervalSeconds = 60;
-    address childChainGatewayRouter = address(0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7);
-    vm.startBroadcast();
-    ChildToParentRewardRouter router = new ChildToParentRewardRouter({
-        _parentChainTarget: parentChainTarget,
-        _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
-        _childChainGatewayRouter: IChildChainGatewayRouter(childChainGatewayRouter)
-    });
-    console.log("Deployed ChildToParentRewardRouter onto Arb Sepolia at");
-    console.log(address(router));
-    vm.stopBroadcast();
-}
+    function run() public {
+        // deployed via sepolia-deploy.s.sol
+        address parentChainTarget = 0xbFC9513A767F226a5878072E1D7C3bF663D9e297;
+        uint256 minDistributionIntervalSeconds = 60;
+        address childChainGatewayRouter = address(0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7);
+        vm.startBroadcast();
+        ChildToParentRewardRouter router = new ChildToParentRewardRouter({
+            _parentChainTarget: parentChainTarget,
+            _minDistributionIntervalSeconds: minDistributionIntervalSeconds,
+            _childChainGatewayRouter: IChildChainGatewayRouter(childChainGatewayRouter)
+        });
+        console.log("Deployed ChildToParentRewardRouter onto Arb Sepolia at");
+        console.log(address(router));
+        vm.stopBroadcast();
+    }
 }
