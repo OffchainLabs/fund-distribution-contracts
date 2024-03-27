bef5012ffeea3c1c36cf9a91e3d8029c73422449
Henry <11198460+godzillaba@users.noreply.github.com>
2024-03-27 16:30:03 -0400

    implement op

--- src/FeeRouter/ChildToParentRewardRouter.sol
+++ src/FeeRouter/ChildToParentRewardRouter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.16;
 
 import "./DistributionInterval.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import "./OpChildToParentRewardRouter.sol";
+import "./BaseChildToParentRewardRouter.sol";
 
 interface IArbSys {
     function withdrawEth(address destination) external payable returns (uint256);
@@ -21,11 +21,7 @@ interface IChildChainGatewayRouter {
     function calculateL2TokenAddress(address _parentChainTokenAddress) external returns (address);
 }
 
-/// @notice Receives native funds and a single ERC20 funds (set on deployment) on an Arbitrum chain and sends them to a target contract on its parent chain.
-///         Funds can only be sent once every minDistributionIntervalSeconds to prevent griefing
-///         (creating many small values messages that each need to be executed in the outbox).
-///         A send is automatically attempted when native funds are receieved in the receive function.
-/// @dev    For native only (i.e., no token), deploy with parentChainTokenAddress, childChainTokenAddress, and childChainGatewayRouter == address(1).
+/// @notice Child to Parent Reward Router deployed to Arbitrum chains
 contract ChildToParentRewardRouter is BaseChildToParentRewardRouter {
     // address of gateway router on this chain
     IChildChainGatewayRouter public immutable childChainGatewayRouter;
@@ -40,7 +36,14 @@ contract ChildToParentRewardRouter is BaseChildToParentRewardRouter {
         address _parentChainTokenAddress,
         address _childChainTokenAddress,
         address _childChainGatewayRouter
-    ) BaseChildToParentRewardRouter(_parentChainTarget, _minDistributionIntervalSeconds, _parentChainTokenAddress, _childChainTokenAddress) {
+    )
+        BaseChildToParentRewardRouter(
+            _parentChainTarget,
+            _minDistributionIntervalSeconds,
+            _parentChainTokenAddress,
+            _childChainTokenAddress
+        )
+    {
         childChainGatewayRouter = IChildChainGatewayRouter(_childChainGatewayRouter);
 
         // If a token is enabled, include token sanity checks
@@ -61,11 +64,11 @@ contract ChildToParentRewardRouter is BaseChildToParentRewardRouter {
         }
     }
 
-    function _withdrawNative(uint256 amount) internal override {
+    function _sendNative(uint256 amount) internal override {
         IArbSys(address(100)).withdrawEth{value: amount}(parentChainTarget);
     }
 
-    function _withdrawToken(uint256 amount) internal override {
+    function _sendToken(uint256 amount) internal override {
         // get gateway from gateway router
         address gateway = childChainGatewayRouter.getGateway(parentChainTokenAddress);
         // approve for transfer, adding 1 so storage slot doesn't get set to 0, saving gas.
