d553ef093208cc8a0e73489bce7f7de9f74bca31
Henry <11198460+godzillaba@users.noreply.github.com>
2025-08-08 11:41:40 -0700

    use stable foundry (#37)
    * use stable foundry
    
    * remove --pretty from inspect script
    
    * uncomment e2e ci
    
    * yarn fix

--- test/util/forge-inspect.bash
+++ test/util/forge-inspect.bash
@@ -18,12 +18,12 @@ for contractName in $contracts; do
 
     # if the file doesn't exist, create it
     if [ ! -f "$outputDir/$contractName" ]; then
-        forge inspect "$contractName" "$inspectType" --pretty > "$outputDir/$contractName"
+        forge inspect "$contractName" "$inspectType" > "$outputDir/$contractName"
         CHANGED=1
     # if the file does exist, compare it        
     else
         mv "$outputDir/$contractName" "$outputDir/$contractName-old"
-        forge inspect "$contractName" "$inspectType" --pretty > "$outputDir/$contractName"
+        forge inspect "$contractName" "$inspectType" > "$outputDir/$contractName"
         diff "$outputDir/$contractName-old" "$outputDir/$contractName"
         if [[ $? != "0" ]]; then
             CHANGED=1
